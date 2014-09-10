d327f2504eebf72a5dd6213a66bd8553ba1409d8
Luke Brooker <mail@lukebrooker.com>
2014-09-11 09:49:59 +1000

    Restructure gulpfile to make paths easier to understand and use

---
+++ gulpfile.paths.js
@@ -0,0 +1,45 @@
+/**
+ * Path Definitions
+ */
+
+paths = {};
+
+paths.app = './app',
+paths.build =  './build';
+paths.bower = './bower_main';
+paths.css = {
+  app: paths.app + '/app.css',
+  bower: paths.bower + '/**/*.css',
+};
+paths.fonts = {
+  bower: paths.bower + '/**/fonts/**/*'
+};
+paths.images = {
+  app: [
+    paths.app + '/**/*.svg',
+    paths.app + '/**/*.jpg',
+    paths.app + '/**/*.png',
+    paths.app + '/**/*.gif'
+  ],
+  bower: paths.bower + '/**/img/**/*'
+};
+paths.js = {
+  app: [paths.app + '/app.js', paths.app + '/**/*.js'],
+  bower: [
+    paths.bower + '/**/angular.js',
+    paths.bower + '/**/*.js'
+  ],
+};
+paths.modernizr = {
+  src: paths.bower + '/modernizr/modernizr.js',
+  build: paths.bower + '/modernizr',
+  buildName: 'modernizr-custom.js'
+};
+paths.templates = ['!' + paths.app + '/index.html', paths.app + '/**/*.html'],
+paths.translations = {
+  build: paths.build + '/translations',
+  po: paths.app + '/components/translations',
+  src: paths.app + '/**/*.html'
+};
+
+module.exports = paths;
