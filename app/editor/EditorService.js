7f71f6182c58c0a06b4bcde8f5d3ec00eb37515c
Luke Brooker <mail@lukebrooker.com>
2014-09-29 14:00:35 +1000

    Move save events to editor service

--- app/editor/EditorService.js
+++ app/editor/EditorService.js
@@ -6,8 +6,11 @@
    * //TODO: parse editorContext in functions
    * @ngInject
    */
-  function EditorService() {
-    var editorService = this;
+  function EditorService($rootScope, $resource, _, UrlService,
+    EventService, PhraseService, DocumentService, MessageHandler,
+    TransStatusService) {
+    var editorService = this,
+        queue = {};
 
     editorService.context = {};
 
@@ -41,6 +44,86 @@
         editorService.context.localeId = localeId;
       }
     };
+
+    /**
+     * EventService.EVENT.SAVE_TRANSLATION listener
+     * Perform save translation with given state
+     *
+     * - queue save translation request (1 global queue, 1 for each TU)
+     * - if queue contains request id, replace old request with new request
+     */
+    $rootScope.$on(EventService.EVENT.SAVE_TRANSLATION,
+      function (event, data) {
+        var phrase = data.phrase,
+          state = data.state;
+
+        //update pending queue if contains
+        if(_.has(queue,  phrase.id)) {
+          var pendingRequest = queue[phrase.id];
+          pendingRequest.phrase = phrase;
+          pendingRequest.state = state;
+          processSaveRequest(phrase.id);
+        } else if(isTranslationModified(phrase)) {
+          state = resolveTranslationState(phrase, state);
+          queue[phrase.id] = {
+            'phrase': phrase,
+            'state' : state,
+            'locale': data.locale,
+            'docId' : data.docId
+          };
+          processSaveRequest(phrase.id);
+        }
+      });
+
+    // Process save translation request
+    function processSaveRequest(id) {
+      var request = queue[id];
+
+      var Translation = $resource(UrlService.TRANSLATION_URL, {}, {
+        update: {
+          method: 'PUT',
+          params: {
+            localeId: request.locale
+          }
+        }
+      });
+
+      var data = {
+        id: request.phrase.id,
+        revision: request.phrase.revision,
+        content: request.phrase.newTranslation,
+        state: request.state,
+        plurals: []
+      };
+
+      Translation.update(data).$promise.then(
+        function(response) {
+          var oldState =  request.phrase.status;
+
+          PhraseService.onTransUnitUpdated(data.id, request.locale,
+            response.revision, response.state, request.phrase.newTranslation);
+
+          DocumentService.updateStatistic(request.docId, request.locale,
+            oldState, response.state, request.phrase.wordCount);
+        },
+        function(response) {
+          MessageHandler.displayWarning('Update translation failed for ' +
+            data.id + ' -' + response);
+          PhraseService.onTransUnitUpdateFailed(data.id);
+        });
+      delete queue[id];
+    }
+
+    function resolveTranslationState(phrase, requestStatus) {
+      if(phrase.newTranslation === '') {
+        return TransStatusService.getId('UNTRANSLATED');
+      }
+      return requestStatus;
+    }
+
+    function isTranslationModified(phrase) {
+      return phrase.newTranslation !== phrase.translation;
+    }
 
     return editorService;
   }
