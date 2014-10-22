5c7e9af5baf61f2affb0aa7f289cbd1198cf8207
Patrick Huang <pahuang@redhat.com>
2014-10-22 12:38:34 +1000

    refactor and enables lagacy shortcuts

--- app/editor/EditorShortcuts.js
+++ app/editor/EditorShortcuts.js
@@ -9,28 +9,155 @@
   function EditorShortcuts($rootScope, EventService, $stateParams,
                                _, hotkeys,
                                $timeout, TransStatusService) {
-    var editorShortcuts = this;
-
-    // mod will be replaced by ctrl if on windows/linux or cmd if on mac
-    editorShortcuts.SHORTCUTS = {
-      COPY_SOURCE: 'alt+g',
-      CANCEL_EDIT: 'esc',
-      SAVE_AS_CURRENT_STATUS: 'mod+s',
-      GOTO_NEXT_ROW: 'tab',
-      GOTO_PREVIOUS_ROW: 'shift+tab',
-      GOTO_NEXT_UNTRANSLATED: 'tab+u',
-      SAVE_AS_NEEDS_WORK: 'mod+shift+s n'
-    };
+    var editorShortcuts = this,
+      copySourceCallback,
+      gotoNextRowCallback,
+      gotoPreviousRowCallback,
+      cancelEditCallback,
+      saveAsCurrentStatusCallback,
+      saveAsModeCallback,
+      gotoToNextUntranslatedCallback;
 
     editorShortcuts.currentPhrase = false;
 
-//    $rootScope.$on(EventService.EVENT.SELECT_TRANS_UNIT, enableEditorKeys);
+    copySourceCallback = function(event) {
+      if (editorShortcuts.currentPhrase) {
+        event.preventDefault();
+        EventService.emitEvent(
+          EventService.EVENT.COPY_FROM_SOURCE, editorShortcuts.currentPhrase);
+      }
+    };
 
-//    $rootScope.$on(EventService.EVENT.CANCEL_EDIT, disableEditorKey);
+    gotoNextRowCallback = function (event) {
+      if (editorShortcuts.currentPhrase) {
+        event.preventDefault();
+        event.stopPropagation();
+        EventService.emitEvent(EventService.EVENT.GOTO_NEXT_ROW,
+          currentContext(editorShortcuts.currentPhrase));
+      }
+    };
 
+    gotoPreviousRowCallback = function (event) {
+      if (editorShortcuts.currentPhrase) {
+        event.preventDefault();
+        EventService.emitEvent(EventService.EVENT.GOTO_PREVIOUS_ROW,
+          currentContext());
+      }
+    };
 
-    editorShortcuts.enableEditorKeys = function enableEditorKeys() {
-      if (!hotkeys.get(editorShortcuts.SHORTCUTS.COPY_SOURCE)) {
+    cancelEditCallback = function (event) {
+      if (editorShortcuts.currentPhrase) {
+        event.preventDefault();
+        event.stopPropagation();
+        EventService.emitEvent(EventService.EVENT.CANCEL_EDIT,
+          editorShortcuts.currentPhrase);
+      }
+    };
+
+    saveAsCurrentStatusCallback = function (event) {
+      if (editorShortcuts.currentPhrase) {
+        event.preventDefault();
+        EventService.emitEvent(EventService.EVENT.SAVE_TRANSLATION,
+          {
+            'phrase': editorShortcuts.currentPhrase,
+            'status': editorShortcuts.currentPhrase.status,
+            'locale': $stateParams.localeId,
+            'docId': $stateParams.docId
+          });
+      }
+    };
+
+    saveAsModeCallback = function (event) {
+      var phrase = editorShortcuts.currentPhrase;
+      if (phrase) {
+        event.preventDefault();
+        EventService.emitEvent(EventService.EVENT.TOGGLE_SAVE_OPTIONS,
+          {
+            'id': phrase.id,
+            'open': true
+          });
+        addSaveAsModeExtensionKey(phrase, 'n', 'needsWork');
+        addSaveAsModeExtensionKey(phrase, 't', 'translated');
+        addSaveAsModeExtensionKey(phrase, 'a', 'approved');
+        $timeout(cancelSaveAsMode, 1000, true);
+      }
+    };
+
+    gotoToNextUntranslatedCallback = function (event) {
+      if (editorShortcuts.currentPhrase) {
+        event.preventDefault();
+        event.stopPropagation();
+        EventService.emitEvent(EventService.EVENT.GOTO_NEXT_UNTRANSLATED,
+          currentContext());
+      }
+    };
+
+    // mod will be replaced by ctrl if on windows/linux or cmd if on mac
+    editorShortcuts.SHORTCUTS = {
+      COPY_SOURCE: new ShortcutInfo(
+        'alt+c', copySourceCallback, 'Copy source as translation', 'alt+g'),
+
+      CANCEL_EDIT: new ShortcutInfo('esc', cancelEditCallback, 'Cancel edit'),
+
+      SAVE_AS_CURRENT_STATUS: new ShortcutInfo(
+        'mod+s', saveAsCurrentStatusCallback, 'Save as current status'),
+
+      GOTO_NEXT_ROW: new ShortcutInfo(
+        'tab', gotoNextRowCallback, 'Move to next row', ['alt+k', 'alt+down'],
+        'keyup'),
+
+      GOTO_PREVIOUS_ROW: new ShortcutInfo(
+        'shift+tab', gotoPreviousRowCallback, 'Move to previous row',
+        ['alt+j', 'alt+up']),
+
+      GOTO_NEXT_UNTRANSLATED: new ShortcutInfo(
+        'tab+u', gotoToNextUntranslatedCallback),
+
+      SAVE_AS_MODE: new ShortcutInfo(
+        'mod+shift+s', saveAsModeCallback, '')
+    };
+
+    /**
+     *
+     * @param defaultKey default key combo for a shortcut
+     * @param callback callback to execute
+     * @param description optional. If not empty will apply to default key (shows in cheatsheet)
+     * @param otherKeys optional other keys that will do the same (won't show in cheatsheet)
+     * @param action optional event to listen to. i.e. 'keyup' or 'keydown'
+     * @returns {EditorShortcuts.ShortcutInfo}
+     * @constructor
+     */
+    function ShortcutInfo(defaultKey, callback, description, otherKeys, action)
+    {
+      this.defaultKey = defaultKey;
+      this.keyCombos = [
+        singleKeyConfig(defaultKey, description, action, callback)
+      ];
+      if (otherKeys) {
+        this.otherKeys = otherKeys instanceof Array ? otherKeys : [otherKeys];
+        this.keyCombos.push(
+          singleKeyConfig(this.otherKeys, '', action, callback));
+      }
+      return this;
+    }
+
+    function singleKeyConfig(combo, description, action, callback) {
+      var keyConfig = {
+        allowIn: ['TEXTAREA'],
+        callback: callback
+      };
+      keyConfig.combo = combo;
+      if (description) {
+        keyConfig.description = description;
+      }
+      if (action) {
+        keyConfig.action = action;
+      }
+      return keyConfig;
+    }
+
+    editorShortcuts.enableEditorKeys = function () {
+      if (!hotkeys.get(editorShortcuts.SHORTCUTS.COPY_SOURCE.defaultKey)) {
         editorShortcuts.enableCancelEditKey();
         editorShortcuts.enableCopySourceKey();
         editorShortcuts.enableGoToNextRowKey();
@@ -38,139 +165,55 @@
         editorShortcuts.enableGoToPreviousRowKey();
         editorShortcuts.enableSaveAsCurrentKey();
         editorShortcuts.enableSaveAsModeKey();
-        editorShortcuts.editorKeysEnabled = true;
       }
     };
 
-    function disableEditorKey() {
+    editorShortcuts.disableEditorKey = function () {
       _.forOwn(editorShortcuts.SHORTCUTS, function(value) {
-        hotkeys.del(value);
-      });
-    }
-
-    editorShortcuts.enableCopySourceKey = function() {
-      hotkeys.add({
-        combo: editorShortcuts.SHORTCUTS.COPY_SOURCE,
-        description: 'Copy source as translation',
-        allowIn: ['INPUT', 'TEXTAREA'],
-        callback: function (event) {
-          if (editorShortcuts.currentPhrase) {
-            event.preventDefault();
-            EventService.emitEvent(
-              EventService.EVENT.COPY_FROM_SOURCE, editorShortcuts.currentPhrase);
-          }
-        }
+        hotkeys.del(value.keyCombos);
       });
     };
 
+    editorShortcuts.enableCopySourceKey = function() {
+      enableShortcut(editorShortcuts.SHORTCUTS.COPY_SOURCE);
+    };
+
+    function enableShortcut(shortcutInfo) {
+      if (!hotkeys.get(shortcutInfo.defaultKey)) {
+        _.forEach(shortcutInfo.keyCombos,
+          function(combo) {
+            hotkeys.add(combo);
+          });
+      }
+    }
+
     editorShortcuts.enableCancelEditKey = function() {
-      hotkeys.add({
-        combo: editorShortcuts.SHORTCUTS.CANCEL_EDIT,
-        description: 'Cancel edit',
-        allowIn: ['INPUT', 'TEXTAREA'],
-        callback: function (event) {
-          if (editorShortcuts.currentPhrase) {
-            event.preventDefault();
-            event.stopPropagation();
-            EventService.emitEvent(EventService.EVENT.CANCEL_EDIT, editorShortcuts.currentPhrase);
-          }
-        }
-      });
+      enableShortcut(editorShortcuts.SHORTCUTS.CANCEL_EDIT);
     };
 
     editorShortcuts.enableSaveAsCurrentKey = function() {
-      hotkeys.add({
-        combo: editorShortcuts.SHORTCUTS.SAVE_AS_CURRENT_STATUS,
-        description: 'Save as current status',
-        allowIn: ['INPUT', 'TEXTAREA'],
-        callback: function (event) {
-          if (editorShortcuts.currentPhrase) {
-            event.preventDefault();
-            EventService.emitEvent(EventService.EVENT.SAVE_TRANSLATION,
-              {
-                'phrase': editorShortcuts.currentPhrase,
-                'status': editorShortcuts.currentPhrase.status,
-                'locale': $stateParams.localeId,
-                'docId': $stateParams.docId
-              });
-          }
-        }
-      });
+      enableShortcut(editorShortcuts.SHORTCUTS.SAVE_AS_CURRENT_STATUS);
     };
 
+    /**
+     * This is to mimic sequence shortcut.
+     * i.e. press ctlr-shift-s then within 1 second, press 'n' to save as
+     * 'needs work'.
+     */
     editorShortcuts.enableSaveAsModeKey = function() {
-      hotkeys.add({
-        combo: editorShortcuts.SHORTCUTS.SAVE_AS_NEEDS_WORK,
-        description: 'Save as certain state',
-        allowIn: ['INPUT', 'TEXTAREA'],
-        callback: function (event) {
-          if (editorShortcuts.currentPhrase) {
-            event.preventDefault();
-            EventService.emitEvent(EventService.EVENT.SAVE_TRANSLATION,
-              {
-                'phrase': editorShortcuts.currentPhrase,
-                'status': TransStatusService.getStatusInfo('needsWork'),
-                'locale': $stateParams.localeId,
-                'docId': $stateParams.docId
-              });
-//            addSaveAsModeExtensionKey(phrase, 'n', 'needsWork');
-//            addSaveAsModeExtensionKey(phrase, 't', 'translated');
-//            addSaveAsModeExtensionKey(phrase, 'a', 'approved');
-//            TransUnitService.toggleSaveAsOptions(true);
-//            $timeout(cancelSaveAsMode, 1000, true);
-          }
-        }
-      });
+      enableShortcut(editorShortcuts.SHORTCUTS.SAVE_AS_MODE);
     };
 
     editorShortcuts.enableGoToNextRowKey = function() {
-      hotkeys.add({
-        combo: editorShortcuts.SHORTCUTS.GOTO_NEXT_ROW,
-        description: 'Go to next row',
-        allowIn: ['INPUT', 'TEXTAREA'],
-        /* it has to be on keyup otherwise other keys using tab as combo will
-         not work*/
-        action: 'keyup',
-        callback: function (event) {
-          if (editorShortcuts.currentPhrase) {
-            event.preventDefault();
-            event.stopPropagation();
-            EventService.emitEvent(EventService.EVENT.GOTO_NEXT_ROW,
-              currentContext(editorShortcuts.currentPhrase));
-          }
-        }
-      });
+      enableShortcut(editorShortcuts.SHORTCUTS.GOTO_NEXT_ROW);
     };
 
     editorShortcuts.enableGoToPreviousRowKey = function() {
-      hotkeys.add({
-        combo: editorShortcuts.SHORTCUTS.GOTO_PREVIOUS_ROW,
-        description: 'Go to previous row',
-        allowIn: ['INPUT', 'TEXTAREA'],
-        callback: function (event) {
-          if (editorShortcuts.currentPhrase) {
-            event.preventDefault();
-            EventService.emitEvent(EventService.EVENT.GOTO_PREVIOUS_ROW,
-              currentContext());
-          }
-        }
-      });
+      enableShortcut(editorShortcuts.SHORTCUTS.GOTO_PREVIOUS_ROW);
     };
 
     editorShortcuts.enableGoToNextUntranslatedKey = function() {
-      hotkeys.add({
-        combo: editorShortcuts.SHORTCUTS.GOTO_NEXT_UNTRANSLATED,
-        description: 'Go to next untranslated row',
-        allowIn: ['INPUT', 'TEXTAREA'],
-        callback: function (event) {
-          if (editorShortcuts.currentPhrase) {
-            event.preventDefault();
-            event.stopPropagation();
-            EventService.emitEvent(EventService.EVENT.GOTO_NEXT_UNTRANSLATED,
-              currentContext());
-          }
-        }
-      });
+      enableShortcut(editorShortcuts.SHORTCUTS.GOTO_NEXT_UNTRANSLATED);
     };
 
     function currentContext() {
@@ -185,10 +228,8 @@
 
     function addSaveAsModeExtensionKey(phrase, combo, status) {
       var statusInfo = TransStatusService.getStatusInfo(status);
-
       hotkeys.add({
         combo: combo,
-        description: 'Save as ' + statusInfo.NAME,
         allowIn: ['INPUT', 'TEXTAREA'],
         action: 'keydown',
         callback: function (event) {
@@ -201,19 +242,20 @@
               'locale': $stateParams.localeId,
               'docId': $stateParams.docId
             });
-          cancelSaveAsMode(combo);
+          cancelSaveAsMode();
         }
       });
     }
 
-    function cancelSaveAsMode(combo) {
-      if (combo) {
-        hotkeys.del(combo);
-      } else {
-        // cancel by timeout
-        hotkeys.del('n');
-      }
-//      TransUnitService.toggleSaveAsOptions(false);
+    function cancelSaveAsMode() {
+      hotkeys.del('n');
+      hotkeys.del('t');
+      hotkeys.del('a');
+      EventService.emitEvent(EventService.EVENT.TOGGLE_SAVE_OPTIONS,
+        {
+          'id': editorShortcuts.currentPhrase.id,
+          'open': false
+        });
     }
 
     return editorShortcuts;
@@ -223,4 +265,6 @@
     .module('app')
     .factory('EditorShortcuts', EditorShortcuts);
 })();
+
+
 
