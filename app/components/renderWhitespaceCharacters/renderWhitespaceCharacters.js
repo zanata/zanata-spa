e8c9e9e0dfe89bfc7cecd5f666cbda8f090b9cf0
Luke Brooker <mail@lukebrooker.com>
2015-01-27 11:17:28 +1000

    Update tab style to fixed width

--- app/components/renderWhitespaceCharacters/renderWhitespaceCharacters.js
+++ app/components/renderWhitespaceCharacters/renderWhitespaceCharacters.js
@@ -19,7 +19,7 @@
       },
       'tab' : {
         'regex' : /\t/g,
-        'template' : '<span class="u-textTab"></span>\t'
+        'template' : '<span class="u-textTab">\t</span>'
       }
     };
 
