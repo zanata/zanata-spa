d59e0186e168798abb7f5ebea46347bfc1cdef3d
David Mason <drdmason@gmail.com>
2015-06-03 22:58:45 +1000

    Prevent excessive REST calls for text search

--- app/components/suggestions/TextSuggestionsService.js
+++ app/components/suggestions/TextSuggestionsService.js
@@ -7,8 +7,10 @@
    */
   function TextSuggestionsService(_, EventService, SuggestionsService,
                                     $rootScope, $timeout) {
-    /* @type {boolean} */
-    var loading = false;
+
+    /* Minimum time in milliseconds to wait between requesting results */
+    var DELAY = 300;
+    var MAX_ACTIVE_REQUESTS = 3;
 
     /* @type {string} */
     var searchText = null;
@@ -16,15 +18,11 @@
     /* @type {Array<Suggestion>} */
     var results = [];
 
-
-    var currentSearchHandle;
-
-
     /**
      * @return {boolean} true if results have been requested and not delivered
      */
     function isLoading() {
-      return loading;
+      return activeRequests > 0;
     }
 
     /**
@@ -45,48 +43,154 @@
       return results;
     }
 
-    $rootScope.$on(EventService.EVENT.REQUEST_TEXT_SUGGESTIONS,
-      function (event, data) {
 
-        if (searchText && searchText === data) {
-          // Same text, no need to repeat search.
+
+    var pendingSearchHandle = null;
+
+    /* Number of requests that are in progress */
+    var activeRequests = 0;
+    /* Time that most recent search was started */
+    var latestSearchTimestamp = Date.now();
+    var latestResultsTimestamp = Date.now();
+
+    /* Search text for a pending search. Will be overwritten whenever a new
+     * search is queued */
+    var pendingSearch = null;
+
+    /**
+     * Make this the next search that will occur when a search is eligible, and
+     * ensure that an appropriate timer is running to initiate the pending
+     * search.
+     *
+     * @param {string} searchText
+     */
+    function deferSearch(searchText) {
+      pendingSearch = searchText;
+      if (pendingSearchHandle) {
+        // timer is already running, no need to start
+        return;
+      }
+
+      // no timer yet, start one
+      waitToRunPendingSearch();
+    }
+
+    function waitToRunPendingSearch() {
+      var eligibleSearchTime = latestSearchTimestamp + DELAY;
+      var timeUntilCanSearch = eligibleSearchTime - Date.now();
+
+      var delay = timeUntilCanSearch > 0 ? timeUntilCanSearch : DELAY;
+
+      pendingSearchHandle = $timeout(function () {
+        pendingSearchHandle = null;
+
+        if (activeRequests >= MAX_ACTIVE_REQUESTS) {
+          // too many requests, keep waiting
+          waitToRunPendingSearch();
           return;
         }
 
-        // a new search will happen, cancel any pending search
-        $timeout.cancel(currentSearchHandle);
+        // run the actual search
+        runPendingSearch();
+      }, delay);
+    }
+
+    /**
+     * Initiate the pending search, and set appropriate variables.
+     */
+    function runPendingSearch() {
+      if (pendingSearch === null) {
+        // no pending search, skip
+        return;
+      }
+
+      var search = pendingSearch;
+      pendingSearch = null;
+      $timeout.cancel(pendingSearchHandle);
+      pendingSearchHandle = null;
+
+      searchByText(search);
+    }
+
+    /**
+     * Perform a search, and set appropriate variables.
+     *
+     * @param {string} text
+     */
+    function searchByText(text) {
+      searchText = text;
+      var timestamp = Date.now();
+      latestSearchTimestamp = timestamp;
+      activeRequests++;
+
+      // Run the search and notify listeners when it is done
+      SuggestionsService.getSuggestionsForText(text).then(
+        function (suggestions) {
+          // Only update results if this search is more recent than the
+          // current results.
+          if (timestamp > latestResultsTimestamp) {
+            latestResultsTimestamp = timestamp;
+            results = suggestions;
+          }
+        },
+        function (error) {
+          console.error('Error searching for text ', error);
+        }).finally(function () {
+          activeRequests--;
+          $rootScope.$broadcast('TextSuggestionsService:updated');
+          if (activeRequests < MAX_ACTIVE_REQUESTS) {
+            runPendingSearch();
+          }
+        });
+    }
+
+
+    $rootScope.$on(EventService.EVENT.REQUEST_TEXT_SUGGESTIONS,
+      function (event, data) {
+
+        if (pendingSearch && pendingSearch === data) {
+          // search already pending
+          return;
+        }
+
+        if (!pendingSearch && activeRequests == 0 && searchText === data) {
+          // search is identical and there are no other searches to replace it
+          return;
+        }
 
         // Empty search can update immediately
         if (data === '') {
           searchText = data;
-          loading = false;
+          //loading = false;
           results = [];
+
+          // Ensure that earlier active searches will not overwrite results.
+          pendingSearch = null;
+          $timeout.cancel(pendingSearchHandle);
+          latestSearchTimestamp = Date.now;
+          latestResultsTimestamp = Date.now();
+
           $rootScope.$broadcast('TextSuggestionsService:updated');
           return;
         }
 
-        currentSearchHandle = $timeout(function() {
-          // Update everything and notify listeners of the update
-          searchText = data;
-          loading = true;
-          results = [];
-          $rootScope.$broadcast('TextSuggestionsService:updated');
+        if (activeRequests >= MAX_ACTIVE_REQUESTS) {
+          // too many requests, queue this one instead
+          deferSearch(data);
+          return;
+        }
 
-          // TODO keep a list of promises for searches that have been started
-          // TODO when a more recent search returns, cancel earlier searches
+        var eligibleSearchTime = latestSearchTimestamp + DELAY;
 
-          // Run the search and notify listeners when it is done
-          SuggestionsService.getSuggestionsForText(data).then(
-            function (suggestions) {
-              results = suggestions;
-              loading = false;
-              $rootScope.$broadcast('TextSuggestionsService:updated');
-            },
-            function (error) {
-              console.error('Error searching for text ', error);
-            });
-        }, 300);
+        if (Date.now() < eligibleSearchTime) {
+          // Too early to search, defer the search
+          deferSearch(data);
+          return;
+        }
 
+        results = [];
+        $rootScope.$broadcast('TextSuggestionsService:updated');
+        searchByText(data);
       });
 
     return {
