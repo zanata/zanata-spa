698a5342524c52bd50281c529a0259601054020c
David Mason <drdmason@gmail.com>
2015-06-27 12:36:00 +1000

    Respond to witty comment.

--- app/AppConstants.js
+++ app/AppConstants.js
@@ -4,6 +4,8 @@
   /**
    * AddConstants
    * "Global" app variables. Don't worry David, they're not really global.
+   * We could probably call them something like "app-global".
+   * As long as they are immutable and in a scope we control, all is fine.
    */
   angular
     .module('app')
