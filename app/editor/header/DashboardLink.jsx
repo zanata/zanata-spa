ed3379e8baf58b0b029c4899d35ad6301df9b303
David Mason <drdmason@gmail.com>
2015-09-10 10:23:31 +1000

    refactor: convert gravatar dashboard link to React component

---
+++ app/editor/header/DashboardLink.jsx
@@ -0,0 +1,33 @@
+/* global React, DashboardLink */
+
+/**
+ * Gravatar icon that links to the dashboard page
+ */
+DashboardLink = React.createClass({
+
+  propTypes: () => {
+    return {
+      dashboardUrl: React.PropTypes.string.isRequired,
+      name: React.PropTypes.string.isRequired,
+      gravatarUrl: React.PropTypes.string
+    };
+  },
+
+  getDefaultProps: () => {
+    return {
+      // default "mystery man" icon
+      gravatarUrl: 'http://www.gravatar.com/avatar/?d=mm'
+    };
+  },
+
+  render: function() {
+    return (
+        <a href={this.props.dashboardUrl}
+           className="u-sizeHeight-2 u-sizeWidth-1_1-2 u-inlineBlock"
+           title={this.props.name}>
+          <img className="u-round Header-avatar"
+            src={this.props.gravatarUrl}/>
+        </a>
+    );
+  }
+});
