f99865efc64b1c33e783f1a8f3934d2fb46bf16e
David Mason <drdmason@gmail.com>
2015-09-13 17:08:53 +1000

    refactor: replace stats bar with React component.

---
+++ app/components/progressbar/ProgressBar.jsx
@@ -0,0 +1,102 @@
+/* global React, ProgressBar, _ */
+
+/**
+ * Bar showing translation progress
+ */
+ProgressBar = React.createClass({
+
+  propTypes: {
+    size: React.PropTypes.string,
+
+    // FIXME stats API gives strings, change those to numbers
+    //       and remove the string option.
+    stats: React.PropTypes.shape({
+      // TODO better to derive total from the others rather than duplicate
+      total: React.PropTypes.oneOfType(
+        [React.PropTypes.number, React.PropTypes.string]),
+      approved: React.PropTypes.oneOfType(
+        [React.PropTypes.number, React.PropTypes.string]),
+      translated: React.PropTypes.oneOfType(
+        [React.PropTypes.number, React.PropTypes.string]),
+      needswork: React.PropTypes.oneOfType(
+        [React.PropTypes.number, React.PropTypes.string]),
+      untranslated: React.PropTypes.oneOfType(
+        [React.PropTypes.number, React.PropTypes.string ])
+    }).isRequired
+  },
+
+  getDefaultProps: () => {
+    return {
+      stats: {
+        total: 0,
+        approved: 0,
+        translated: 0,
+        needswork: 0,
+        untranslated: 0
+      }
+    };
+  },
+
+  ProgressItem: React.createClass({
+    propTypes: {
+      state: React.PropTypes.string.isRequired,
+      start: React.PropTypes.number.isRequired,
+      width: React.PropTypes.number.isRequired
+    },
+    render: function () {
+      let classes = 'Progressbar-item Progressbar-' + this.props.state;
+      let style = {
+        marginLeft: this.props.start + '%',
+        width: this.props.width + '%'
+      };
+      return (
+        <span className={classes}
+          style={style}/>
+      );
+    }
+  }),
+
+  render: function() {
+    let ProgressItem = this.ProgressItem;
+    let classes = 'Progressbar'
+      + (this.props.size === 'small' ? ' Progressbar--sm' : '')
+      + (this.props.size === 'large' ? ' Progressbar--lg' : '');
+    let stats = this.props.stats;
+
+    let total = parseFloat(stats.total);
+    let widths = _.chain(stats)
+      .pick(['approved', 'translated', 'needswork', 'untranslated'])
+      .mapValues((count) => {
+        return count ? 100 * parseFloat(count) / total : 0;
+      })
+      .value();
+
+    var starts = {
+      approved: 0,
+      translated: widths.approved
+    };
+    starts.needswork = starts.translated + widths.translated;
+    starts.untranslated = starts.needswork + widths.needswork;
+
+    return (
+      <div className={classes}>
+        <ProgressItem
+          state="approved"
+          start={starts.approved}
+          width={widths.approved}/>
+        <ProgressItem
+          state="translated"
+          start={starts.translated}
+          width={widths.translated}/>
+        <ProgressItem
+          state="needsWork"
+          start={starts.needswork}
+          width={widths.needswork}/>
+        <ProgressItem
+          state="untranslated"
+          start={starts.untranslated}
+          width={widths.untranslated}/>
+      </div>
+    );
+  }
+});
--- app/components/progressbar/progressbarDirective.js
+++ app/components/progressbar/progressbarDirective.js
@@ -1,75 +1,33 @@
+/* global React, ProgressBar */
 (function() {
   'use strict';
 
   /**
-   * @name progressbar
-   * @description progressbar container
+   * @name message-stats-bar
+   * @description react progress bar component
    * @ngInject
    */
-  function progressbar() {
+  function messageStatsBar() {
     return {
       restrict: 'E',
-      required: 'progressbarStatistic',
-      scope: {
-        statistic: '=progressbarStatistic',
-        size: '@' //large, full, or empty
-      },
-      templateUrl: 'components/progressbar/progressbar.html',
-      controller: function($scope) {
-        /**
-         * Need to set to true for complex object watch. Performance issue.
-         * https://docs.angularjs.org/api/ng/type/$rootScope.Scope
-         */
-        $scope.$watch('statistic', function(statistic) {
-          if (statistic) {
-            $scope.style = getStyle(statistic);
-          }
-        }, true);
+      required: ['editor'],
+      link: function (scope, element) {
+        scope.$watch('editor.messageStatistic', render, true);
+
+        function render() {
+          React.render(
+            React.createElement(ProgressBar, {
+              size: 'small',
+              stats: scope.editor.messageStatistic
+            }),
+            element[0]
+          );
+        }
       }
     };
   }
 
-  function getStyle(statistic) {
-    var total = statistic.total,
-        widthApproved = getWidthPercent(statistic.approved, total),
-        widthTranslated = getWidthPercent(statistic.translated, total),
-        marginLeftTranslated = widthApproved,
-        widthNeedsWork = getWidthPercent(statistic.needswork, total),
-        marginLeftNeedsWork = widthApproved + widthTranslated,
-        widthUntranslated = getWidthPercent(statistic.untranslated, total),
-        marginLeftUntranslated = widthApproved +
-          widthTranslated + widthNeedsWork,
-        style = {};
-
-    style.approved = {
-      'width': widthApproved + '%',
-      'marginLeft': 0
-    };
-    style.translated = {
-      'width': widthTranslated + '%',
-      'marginLeft': marginLeftTranslated + '%'
-    };
-    style.needsWork = {
-      'width': widthNeedsWork + '%',
-      'marginLeft': marginLeftNeedsWork + '%'
-    };
-    style.untranslated = {
-      'width': widthUntranslated + '%',
-      'marginLeft': marginLeftUntranslated + '%'
-    };
-    return style;
-  }
-
-  function getWidthPercent(value, total) {
-    var percent = 0;
-    if (value) {
-      percent = value / total * 100;
-    }
-    return percent;
-  }
-
   angular
     .module('app')
-    .directive('progressbar', progressbar);
-
+    .directive('messageStatsBar', messageStatsBar);
 })();
