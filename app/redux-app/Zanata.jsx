7787188df7bc7c13ac395ff85847b472e150f473
David Mason <drdmason@gmail.com>
2016-01-27 13:25:00 +1000

    WIP: quick & dirty display of phrase status list for selected doc

--- app/redux-app/Zanata.jsx
+++ app/redux-app/Zanata.jsx
@@ -10,17 +10,36 @@ import { requestPhraseList } from './actions'
 class Zanata extends React.Component {
   render () {
     // console.dir(this.props)
-    return <div>
-             Component
-             <div onClick={this.props.requestPhraseList}>"Zanata"</div>
-             . See console for props
-           </div>
+    const phrases = this.props.phrases || []
+    const phraseElements = phrases.map(({id, resId, status}, index) => {
+      return <div key={index}>
+        <table style={{border: '1px solid light gray'}}>
+          <tbody>
+            <tr>
+              <td>index</td><td>{index}</td>
+              <td>id</td><td>{id}</td>
+              <td>resId</td><td>{resId}</td>
+              <td>status</td><td>{status}</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    })
+
+    return (
+      <div>
+        <div onClick={this.props.requestPhraseList}>"Click to get phrases"</div>
+        {phraseElements}
+      </div>
+
+    )
   }
 }
 
-function mapStateToProps (state) {
-  // console.dir(state)
-  return {} // { ...params }
+function mapStateToProps (state, ownProps) {
+  return {
+    phrases: state.phrases.inDoc[ownProps.params.docId]
+  }
 }
 
 function mapDispatchToProps (dispatch, ownProps) {
