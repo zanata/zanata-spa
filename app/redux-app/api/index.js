6d4cb99f128334c251cdacda084554e9c5b44858
David Mason <drdmason@gmail.com>
2016-02-11 15:43:19 +1000

    WIP: restore functions to API module

--- app/redux-app/api/index.js
+++ app/redux-app/api/index.js
@@ -123,6 +123,8 @@ export function fetchProjectInfo(projectSlug) {
   })
 }
 
+// FIXME replace any call to fetchDocumentList with fetchDocuments
+// export function fetchDocumentList (projectSlug, versionSlug) {
 export function fetchDocuments(projectSlug, versionSlug) {
   const docListUrl = `${baseUrl}/project/${projectSlug}/version/${versionSlug}/docs`;
   return fetch(docListUrl, {
@@ -145,4 +147,45 @@ export function fetchVersionLocales(projectSlug, versionSlug) {
     },
     mode: 'cors'
   })
+}
+
+export function savePhrase ({ id, revision, plural },
+                            { localeId, status, translations }) {
+  const translationUrl = `${baseUrl}/trans/${localeId}`
+  return fetch(translationUrl, {
+    method: 'PUT',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    },
+    mode: 'cors',
+    body: JSON.stringify({
+      id,
+      revision, // || 0
+      plural,
+      content: translations[0],
+      contents: translations,
+      // TODO also limit to only valid status
+      status: phraseStatusToTransUnitStatus(status)
+    })
+  })
+}
+
+/**
+ * Convert from lowercase phrase status used in redux app
+ * to the caps-case strings used in the REST interface.
+ */
+function phraseStatusToTransUnitStatus (status) {
+  switch (status) {
+    case 'untranslated':
+      return 'New'
+    case 'needswork':
+      return 'NeedReview'
+    case 'translated':
+      return 'Translated'
+    case 'approved':
+      return 'Approved'
+    default:
+      console.error('Save attempt with invalid status', status)
+  }
 }
